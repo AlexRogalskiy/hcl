d4fe17fa7e2d285b1629f3390e49ebaf1f8d1ac8
Chris Barnes <barnesc@janelia.hhmi.org>
2020-09-07 11:00:58 +0100

    allow help with arguments without file

--- makedocs.sh
+++ makedocs.sh
@@ -5,6 +5,5 @@ hcl --command "help" | pipe2codeblock README.md --tgt _commands
 
 mkdir -p commands
 rm -f commands/*.md
-hcl --command "help" | \
-    awk '{print $1}' | \
+hcl --command "help -s" | \
     xargs --max-procs 4 -I _ sh -c "printf '# _\n\n\`\`\`\n' > commands/_.md && hcl --command '_ --help' >> commands/_.md && echo '\`\`\`' >> commands/_.md"
