2466de0a03a780c73d2263e4640946e6c8cb20eb
Chris Barnes <barnesc@janelia.hhmi.org>
2020-11-19 21:49:29 +0000

    replace makedocs with Makefile

--- makedocs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hcl --help | pipe2codeblock README.md --tgt _help
-hcl --command "help" | pipe2codeblock README.md --tgt _commands
-
-mkdir -p commands
-rm -f commands/*.md
-hcl --command "help -s" | \
-    xargs --max-procs 4 -I _ sh -c "printf '# _\n\n\`\`\`\n' > commands/_.md && hcl --command '_ --help' >> commands/_.md && echo '\`\`\`' >> commands/_.md"
